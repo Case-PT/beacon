c8d806f7fcf37c1a26bc464af7b368f283567182
home <brnli7@gmail.com>
2017-10-11 23:20:58 -0400

    Added DockerFile

--- service/src/main/java/com/sdpt/beacon/service/MainServiceApplication.java
+++ service/src/main/java/com/sdpt/beacon/service/MainServiceApplication.java
@@ -1,9 +1,19 @@
+package com.sdpt.beacon.service;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MainServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
     }
 }
